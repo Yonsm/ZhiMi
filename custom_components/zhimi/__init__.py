f8354577bd9e3ed037ab7b09009f1d78f1546358
Yonsm <Yonsm@qq.com>
2021-03-18 00:40:38 +0800

    Refine dependence related

--- custom_components/zhimi/__init__.py
+++ custom_components/zhimi/__init__.py
@@ -1,23 +1,14 @@
-from homeassistant.core import HomeAssistant
-from homeassistant.helpers.storage import STORAGE_DIR
-from homeassistant.helpers.aiohttp_client import async_get_clientsession
-
-REQUIREMENTS = ['miservice>=2021.3.1']
-
 DOMAIN = 'zhimi'
 
-_mi_account = None
-_miio_service = None
+mi_account = None
+miio_service = None
 
 
-async def async_setup(hass: HomeAssistant, config):
+async def async_setup(hass, config):
     conf = config.get(DOMAIN)
-    global _mi_account, _miio_service
+    global mi_account, miio_service
     from miservice import MiAccount, MiIOService
-    _mi_account = MiAccount(async_get_clientsession(hass), conf['username'], conf['password'], hass.config.path(STORAGE_DIR, DOMAIN))
-    _miio_service = MiIOService(_mi_account, conf.get('region'))
+    mi_account = MiAccount(hass.helpers.aiohttp_client.async_get_clientsession(), conf['username'], conf['password'], hass.config.path(hass.helpers.storage.STORAGE_DIR, DOMAIN))
+    miio_service = MiIOService(mi_account, conf.get('region'))
     return True
 
-
-def get_miio_service():
-    return _miio_service
